2c08ec49e81e165438d375231d80fd3d6405fe4b
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-05-03 22:19:32 +0300

    update class

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,10 +1,7 @@
 package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
-
-    @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
-
+    public void repeatIsBad() {
         System.out.println("writing the same code doesn't have much impact, and " +
                 "it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and " +
@@ -24,7 +21,6 @@ public class RepeatIsBad {
         System.out.println("writing the same code doesn't have much impact, and " +
                 "it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and " +
-                "it's also time consuming");System.out.println("writing the same code doesn't have much impact, and " +
                 "it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and " +
                 "it's also time consuming");
@@ -33,7 +29,10 @@ public class RepeatIsBad {
         System.out.println("writing the same code doesn't have much impact, and " +
                 "it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and " +
-                "it's also time consuming");System.out.println("writing the same code doesn't have much impact, and " +
+                "it's also time consuming");
+        System.out.println("writing the same code doesn't have much impact, and " +
+                "it's also time consuming");
+        System.out.println("writing the same code doesn't have much impact, and " +
                 "it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and " +
                 "it's also time consuming");
